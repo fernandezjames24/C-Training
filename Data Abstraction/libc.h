387ff6a1abc3471858c0f55d8526567f64930f22
fernandezjames24 <50700392+fernandezjames24@users.noreply.github.com>
2019-07-26 16:04:23 +0800

    Create libc.h

---
+++ Data Abstraction/libc.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+class Libc
+{
+	
+private:
+	//std::string a, b, c;
+	std::string s[3];
+public:
+	void geta();
+	void seta();
+
+	void getb();
+	void setb();
+
+	void getc();
+	void setc();
+
+
+};
+
