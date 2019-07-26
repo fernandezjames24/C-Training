51124f12eff88ac875c759fa01c84860a242fc36
fernandezjames24 <50700392+fernandezjames24@users.noreply.github.com>
2019-07-26 16:05:42 +0800

    Create Libc.cpp

---
+++ Data Abstraction/Libc.cpp
@@ -0,0 +1,27 @@
+#include "libc.h"
+#include <string>
+
+
+void Libc::geta() {
+	
+}
+
+void Libc::seta() {
+	
+}
+
+void Libc::getb() {
+	
+}
+
+void Libc::setb() {
+	
+}
+
+void Libc::getc() {
+	
+}
+
+void Libc::setc() {
+	
+}
